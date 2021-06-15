06ff284fec673ef54ab46d41e27f22f0648bd85b
Balazs Marton <balazs.marton@prezi.com>
2021-06-15 21:55:58 +0200

    refact: extract method for creating image button

--- main.py
+++ main.py
@@ -18,8 +18,14 @@ website_label.grid(row=0, column=0, columnspan=2)
 
 website_label = Label(text="word")
 website_label.grid(row=1, column=0, columnspan=2)
-ph = PhotoImage("right.png", width=100, height=100)
-right_button = Button(image=ph)
+
+
+def create_image_button(image_path):
+    image = PhotoImage(image_path, width=100, height=100)
+    return Button(image=image)
+
+
+right_button = create_image_button("right.png")
 right_button.grid(row=2, column=0)
 
 window.mainloop()
