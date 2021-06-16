cfa7b2fa83334cbe7eb375f1c19f0c5e07b67a9f
Balazs Marton <balazs.marton@prezi.com>
2021-06-16 23:29:59 +0200

    bugfix: fix buttons

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tkinter import (
     Tk,
-    Label,
     Button,
     PhotoImage,
     Canvas,
@@ -14,7 +13,7 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526)
 card_front_image = PhotoImage(file="images/card_front.png")
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
 canvas.create_image(400, 263, image=card_front_image)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -23,15 +22,12 @@ canvas.create_text(400, 150, text="Title", font=("Ariel", 48, "italic"))
 canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 
 
-def create_image_button(image_path):
-    image = PhotoImage(file=image_path)
-    return Button(image=image, highlightthickness=0)
-
-
-right_button = create_image_button("images/right.png")
-right_button.grid(row=3, column=0)
-wrong_button = create_image_button("images/wrong.png")
-wrong_button.grid(row=3, column=1)
+right_button_image = PhotoImage(file="images/right.png")
+right_button = Button(image=right_button_image, highlightthickness=0)
+right_button.grid(row=1, column=0)
+wrong_button_image = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_button_image, highlightthickness=0)
+wrong_button.grid(row=1, column=1)
 
 window.mainloop()
 
